814851a78855afb2a711cada09f95022209148a3
Victor Martinez <victor.martinez@versia.com>
2018-09-25 18:28:30 +0200

    getpublic.sh

--- scripts/getpublicIP/install_getpubliIP.sh
+++ scripts/getpublicIP/install_getpubliIP.sh
@@ -10,14 +10,14 @@
 #   Description: Install getpublicIP.sh
 
 ## Distribute script and configuration files.
-sudo cp ./getpublicIP.sh /usr/local/bin/getpublicip.sh
-sudo chmod +x /usr/local/bin/getpublicip.sh
+sudo cp ./getpublicIP.sh /usr/local/bin/getpublicIP.sh
+sudo chmod +x /usr/local/bin/getpublicIP.sh
 sudo cp ./getpublicIP.service /etc/systemd/system/getpublicIP.service
 
 ## Enable and star service
-sudo systemctl enable getpublicip
-sudo systemctl start getpublicip
-sudo systemctl status getpublicip
+sudo systemctl enable getpublicIP
+sudo systemctl start getpublicIP
+sudo systemctl status getpublicIP
 
 echo " "
 echo "check IP ....."
